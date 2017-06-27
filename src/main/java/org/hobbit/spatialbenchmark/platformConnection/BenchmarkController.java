df21d9e7348b81abeb587a862e0c70f3d1358949
jsaveta <jsaveta@gmail.com>
2017-06-27 15:04:19 +0300

    added tomtom mimicking algorithm

--- src/main/java/org/hobbit/spatialbenchmark/platformConnection/BenchmarkController.java
+++ src/main/java/org/hobbit/spatialbenchmark/platformConnection/BenchmarkController.java
@@ -19,13 +19,13 @@ import org.slf4j.LoggerFactory;
 public class BenchmarkController extends AbstractBenchmarkController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BenchmarkController.class);
-//    private static final String DATA_GENERATOR_CONTAINER_IMAGE = "git.project-hobbit.eu:4567/jsaveta1/spatialdatagenerator";
-//    private static final String TASK_GENERATOR_CONTAINER_IMAGE = "git.project-hobbit.eu:4567/jsaveta1/spatialtaskgenerator";
-//    private static final String EVALUATION_MODULE_CONTAINER_IMAGE = "git.project-hobbit.eu:4567/jsaveta1/spatialevaluationmodule";
+    private static final String DATA_GENERATOR_CONTAINER_IMAGE = "git.project-hobbit.eu:4567/jsaveta1/spatialdatagenerator";
+    private static final String TASK_GENERATOR_CONTAINER_IMAGE = "git.project-hobbit.eu:4567/jsaveta1/spatialtaskgenerator";
+    private static final String EVALUATION_MODULE_CONTAINER_IMAGE = "git.project-hobbit.eu:4567/jsaveta1/spatialevaluationmodule";
 
-    private static final String DATA_GENERATOR_CONTAINER_IMAGE = "spatial_data-generator";
-    private static final String TASK_GENERATOR_CONTAINER_IMAGE = "spatial_task-generator";
-    private static final String EVALUATION_MODULE_CONTAINER_IMAGE = "spatial_evaluation-module";
+//    private static final String DATA_GENERATOR_CONTAINER_IMAGE = "spatial_data-generator";
+//    private static final String TASK_GENERATOR_CONTAINER_IMAGE = "spatial_task-generator";
+//    private static final String EVALUATION_MODULE_CONTAINER_IMAGE = "spatial_evaluation-module";
     private String[] envVariablesEvaluationModule;
     
     
