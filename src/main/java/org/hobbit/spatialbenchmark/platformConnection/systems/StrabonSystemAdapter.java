40194fe12fbe3006f1902c86b840450c7ed48827
jsaveta <jsaveta@gmail.com>
2018-05-10 15:46:57 +0300

    implementing Strabon system adapter

--- src/main/java/org/hobbit/spatialbenchmark/platformConnection/systems/StrabonSystemAdapter.java
+++ src/main/java/org/hobbit/spatialbenchmark/platformConnection/systems/StrabonSystemAdapter.java
@@ -7,8 +7,7 @@ package org.hobbit.spatialbenchmark.platformConnection.systems;
 
 import com.rabbitmq.client.ConsumerCancelledException;
 import com.rabbitmq.client.ShutdownSignalException;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
@@ -21,13 +20,12 @@ import org.hobbit.spatialbenchmark.rabbit.SingleFileReceiver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  *
@@ -43,21 +41,12 @@ public class StrabonSystemAdapter extends AbstractSystemAdapter {
     private String dataFormat;
     private String taskFormat;
     private String resultsFile;
-//    private Strabon strabon = null; // An instance of Strabon
     private String db = null; // Spatially enabled PostGIS database where data is stored 
     private String user = null; // Username to connect to database
     private String passwd = null; // Password to connect to database
     private Integer port = null; // Database host to connect to
     private String host = null; // Port to connect to on the database host
     private String url = null;
-    private static String PREFIXES
-            = "PREFIX geof: <http://www.opengis.net/def/function/geosparql/> "
-            + "PREFIX owl: <http://www.w3.org/2002/07/owl#> "
-            + "PREFIX tomtom: <http://www.tomtom.com/ontologies/traces#> "
-            + "PREFIX tomtomregions: <http://www.tomtom.com/ontologies/regions#> "
-            + "PREFIX spaten: <http://www.spaten.com/ontologies/traces#> "
-            + "PREFIX spatenregions: <http://www.spaten.com/ontologies/regions#> "
-            + "PREFIX strdf: <http://strdf.di.uoa.gr/ontology#> "; //fill this and append the query 
 
     @Override
     public void init() throws Exception {
@@ -87,7 +76,7 @@ public class StrabonSystemAdapter extends AbstractSystemAdapter {
             LOGGER.info("Received data from receiveGeneratedData..");
 
         } catch (IOException | ShutdownSignalException | ConsumerCancelledException | InterruptedException ex) {
-            java.util.logging.Logger.getLogger(SilkSystemAdapter.class.getName()).log(Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StrabonSystemAdapter.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
@@ -143,7 +132,7 @@ public class StrabonSystemAdapter extends AbstractSystemAdapter {
 //                LOGGER.error("Exception while sending storage space cost to evaluation storage.", e);
 //            }
 //        } catch (IOException ex) {
-//            java.util.logging.Logger.getLogger(SilkSystemAdapter.class.getName()).log(Level.SEVERE, null, ex);
+//            java.util.logging.Logger.getLogger(StrabonSystemAdapter.class.getName()).log(Level.SEVERE, null, ex);
 //        }
     }
 
@@ -157,51 +146,92 @@ public class StrabonSystemAdapter extends AbstractSystemAdapter {
         String G2 = "http://G2";
 
         try {
-//            String[] envVariablesPostgresql = new String[]{"-p 9999:8080"};
             String[] envVariablesPostgresql = new String[]{};
-            String postgresqlContName = this.createContainer("git.project-hobbit.eu:4567/jsaveta1/strabonsystemadapter/strabon:latest", envVariablesPostgresql);
+            String postgresqlContName = "localhost"; //this.createContainer("git.project-hobbit.eu:4567/jsaveta1/strabonsystemadapter/strabon:latest", envVariablesPostgresql);
             LOGGER.info("postgresqlContName " + postgresqlContName);
 
-            //connect to strabon
-            LOGGER.info("[Strabon] Store and Query to Strabon");
-//          http://www.strabon.di.uoa.gr/files/stSPARQL_tutorial.pdf
-//          queries: http://geographica.di.uoa.gr/queries/joins.txt
-            StringBuilder query = new StringBuilder();
-            query.append(PREFIXES);
-            query.append("\"SELECT ?s1 ?s2"
-                    + " WHERE {GRAPH " + G1 + " {?s1 <http://strdf.di.uoa.gr/ontology#hasGeometry> ?o1}"
-                    + " GRAPH " + G2 + " {?s2 <http://strdf.di.uoa.gr/ontology#hasGeometry> ?o2}"
-                    + " FILTER(geof:sfEquals(?o1, ?o2))."
-                    + " }\";");
+            //////////////////////////////////
+            LOGGER.info("Storing source dataset into G1 graph..");
+            source = System.getProperty("user.dir") + File.separator + source.replace("./", "");
+            String storeScriptFilePath = System.getProperty("user.dir") + File.separator + "store.sh";
+            String[] storeG1 = {"/bin/bash", storeScriptFilePath, postgresqlContName, port.toString(), db, user, passwd, source, taskFormat, G1};
 
-//java.net.UnknownHostException: ..
-//          not all formats are supported! check here:http://hg.strabon.di.uoa.gr/Strabon/file/9ee6935c722a/runtime/src/main/java/eu/earthobservatory/runtime/generaldb/Strabon.java
-            String[] store = {"/bin/sh", "-c", "cd ./strabon/ && java -cp $(for file in `ls -1 *.jar`; do myVar=$myVar./$file\":\";"
-                + "done;echo $myVar;) eu.earthobservatory.runtime.postgis.StoreOp " + postgresqlContName + " " + port + " " + db
-                + " " + user + " " + passwd + " " + "../" + source + " -f " + taskFormat + " -g " + G1 + " && java -cp $(for file in `ls -1 *.jar`; do myVar=$myVar./$file\":\";"
-                + "done;echo $myVar;) eu.earthobservatory.runtime.postgis.StoreOp " + postgresqlContName + " " + port + " " + db
-                + " " + user + " " + passwd + " " + "../" + target + " -f " + taskFormat + " -g " + G2 };
-//                + "&& java -cp $(for file in `ls -1 *.jar`; do myVar=$myVar./$file\":\";"
-//                + "done;echo $myVar;) eu.earthobservatory.runtime.postgis.QueryOp " + postgresqlContName + " " + port + " " + db 
-//                + " " + user + " " + passwd + " " + query.toString() + " SESAME_CSV", "-get t"};
-            Process p = Runtime.getRuntime().exec(store);
-            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            Process pG1 = new ProcessBuilder(storeG1).redirectErrorStream(true).start(); //= Runtime.getRuntime().exec(command);
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(pG1.getInputStream()));
+            BufferedReader stdError = new BufferedReader(new InputStreamReader(pG1.getErrorStream()));
 
-            BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-
-            // read the output from the command
-            LOGGER.info("Here is the standard output of the command:\n");
+            LOGGER.info("Here is the standard output of the command for storing source dataset into G1 graph:\n");
             String out = null;
             while ((out = stdInput.readLine()) != null) {
                 LOGGER.info(out);
             }
 
             // read any errors from the attempted command
-            LOGGER.info("Here is the standard error of the command (if any):\n");
+            LOGGER.info("Here is the standard error of the command for storing source dataset into G1 graph (if any):\n");
             while ((out = stdError.readLine()) != null) {
                 LOGGER.info(out);
             }
 
+            pG1.waitFor();
+            stdInput.close();
+            stdInput.close();
+            LOGGER.info("Store source dataset into G1 graph completed...");
+
+            //////////////////////////////////
+            LOGGER.info("Storing target dataset into G2 graph..");
+
+            target = System.getProperty("user.dir") + File.separator + target.replace("./", "");
+            String[] storeG2 = {"/bin/bash", storeScriptFilePath, postgresqlContName, port.toString(), db, user, passwd, target, taskFormat, G2};
+
+            Process pG2 = new ProcessBuilder(storeG2).redirectErrorStream(true).start(); //= Runtime.getRuntime().exec(command);
+            stdInput = new BufferedReader(new InputStreamReader(pG2.getInputStream()));
+            stdError = new BufferedReader(new InputStreamReader(pG2.getErrorStream()));
+
+            LOGGER.info("Here is the standard output of the command for storing target dataset into G2 graph:\n");
+            out = null;
+            while ((out = stdInput.readLine()) != null) {
+                LOGGER.info(out);
+            }
+
+            // read any errors from the attempted command
+            LOGGER.info("Here is the standard error of the command for storing target dataset into G2 graph (if any):\n");
+            while ((out = stdError.readLine()) != null) {
+                LOGGER.info(out);
+            }
+
+            pG2.waitFor();
+            stdInput.close();
+            stdInput.close();
+            LOGGER.info("Store target dataset into G2 graph completed...");
+
+            //////////////////////////////////
+            LOGGER.info("Query for " + relation + " relation between source and target graphs (G1, G2)..");
+            String query = createQuery(relation, G1, G2);
+            String queryScriptFilePath = System.getProperty("user.dir") + File.separator + "query.sh";
+            String[] queryCMD = {"/bin/bash", queryScriptFilePath, postgresqlContName, port.toString(), db, user, passwd, query, "SESAME_CSV"};
+            Process pQ = new ProcessBuilder(queryCMD).redirectErrorStream(true).start(); //= Runtime.getRuntime().exec(command);
+            stdInput = new BufferedReader(new InputStreamReader(pQ.getInputStream()));
+            stdError = new BufferedReader(new InputStreamReader(pQ.getErrorStream()));
+
+            LOGGER.info("Here is the standard output of the command for the query:\n");
+            out = null;
+            while ((out = stdInput.readLine()) != null) {
+                LOGGER.info(out);
+            }
+
+            // read any errors from the attempted command
+            LOGGER.info("Here is the standard error of the command for the query (if any):\n");
+            while ((out = stdError.readLine()) != null) {
+                LOGGER.info(out);
+            }
+
+            pQ.waitFor();
+            stdInput.close();
+            stdInput.close();
+            LOGGER.info("Query for " + relation + " relation between source and target graphs (G1, G2) completed...");
+
+            //TODO: save results in file.. 
+            
             LOGGER.info("[Strabon] Store and Query to Strabon completed.");
 
         } catch (Exception e) {
@@ -235,6 +265,59 @@ public class StrabonSystemAdapter extends AbstractSystemAdapter {
         LOGGER.info("Strabon System Adapter closed successfully.");
     }
 
+    public static void copy(String sourcePath, String destinationPath) throws IOException {
+        Files.copy(Paths.get(sourcePath), new FileOutputStream(destinationPath));
+    }
+
+    private String createQuery(String relation, String G1, String G2) {
+        //EQUALS, DISJOINT, TOUCHES, CONTAINS, COVERS, INTERSECTS, 
+        //WITHIN, COVERED_BY, CROSSES, OVERLAPS
+        
+        //TODO: fill queries
+        String query = null;
+        String PREFIXES
+                = "PREFIX geof: <http://www.opengis.net/def/function/geosparql/> "
+                + "PREFIX owl: <http://www.w3.org/2002/07/owl#> "
+                + "PREFIX tomtom: <http://www.tomtom.com/ontologies/traces#> "
+                + "PREFIX tomtomregions: <http://www.tomtom.com/ontologies/regions#> "
+                + "PREFIX spaten: <http://www.spaten.com/ontologies/traces#> "
+                + "PREFIX spatenregions: <http://www.spaten.com/ontologies/regions#> "
+                + "PREFIX strdf: <http://strdf.di.uoa.gr/ontology#> ";
+
+        switch (relation) {
+            case "EQUALS":
+                StringBuilder sbQuery = new StringBuilder();
+                sbQuery.append(PREFIXES);
+                sbQuery.append("\"SELECT ?s1 ?s2"
+                        + " WHERE {GRAPH " + G1 + " {?s1 <http://strdf.di.uoa.gr/ontology#hasGeometry> ?o1}"
+                        + " GRAPH " + G2 + " {?s2 <http://strdf.di.uoa.gr/ontology#hasGeometry> ?o2}"
+                        + " FILTER(geof:sfEquals(?o1, ?o2))."
+                        + " }\";");
+                query = sbQuery.toString();
+                break;
+            case "DISJOINT":
+                break;
+            case "TOUCHES":
+                break;
+            case "CONTAINS":
+                break;
+            case "COVERS":
+                break;
+            case "INTERSECTS":
+                break;
+            case "WITHIN":
+                break;
+            case "COVERED_BY":
+                break;
+            case "CROSSES":
+                break;
+            case "OVERLAPS":
+                break;
+
+        }
+        return query;
+    }
+
     private static void help() {
         LOGGER.info("Usage: eu.earthobservatory.runtime.postgis.StoreOp <HOST> <PORT> <DATABASE> <USERNAME> <PASSWORD> <FILE> [-f <FORMAT>] [-g <NAMED_GRAPH>] [-i <INFERENCE>]");
         LOGGER.info("       where <HOST>       		 is the postgis database host to connect to");
@@ -257,4 +340,5 @@ public class StrabonSystemAdapter extends AbstractSystemAdapter {
         LOGGER.info("             <DELET_LOCK> is true when deletion of \"locked\" table should be enforced (e.g., when Strabon has been ungracefully shutdown).");
         LOGGER.info("             [<FORMAT>]   is the format of your results (default: XML)");
     }
+
 }
